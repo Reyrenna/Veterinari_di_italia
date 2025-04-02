7edd8e74060a4454985756cbdf995f41a32c2585
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-04-03 00:17:33 +0200

    funzione anamnesi animale ordine inverso

---
+++ Veterinari_di_italia/DTOs/VisiteVeterinarie/GetAllVisiteByAnimalIdResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinari_di_italia.DTOs.VisiteVeterinarie
+{
+    public class GetAllVisiteByAnimalIdResponseDto
+    {
+        [Required]
+        public required string Message { get; set; }
+
+        [Required]
+        public List<VisiteVeterinarieSimpleDto>? Visite { get; set; }
+    }
+}
